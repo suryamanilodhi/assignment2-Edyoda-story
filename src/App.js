89f784113e80bcae68953e00d076c0b9e8d2a6eb
suryamanilodhi <suryamanilodhi@gmail.com>
2023-05-12 22:23:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import Logo from './Image/edyoda1.jpg'
 import './App.css';
-
+import ImageGrid from './Component/image'
+import {RiArrowDropDownLine} from 'react-icons/ri'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return ( 
+  
+  <div>
+    <header className="header">
+    <section className="first">
+      <div>
+      <img id="logo" src={Logo} alt="react logo"/>
+         <p>Stories</p>
+      </div>
+    
+    <p className='cat'>Explore Categories<span><RiArrowDropDownLine/></span> </p>
+   
+    </section> 
+     <section className="second">
+      <p className='header-p'>EdYoda is a learning and knowledge
+      sharing platform for techies</p> 
+      <button>Go To Main Website</button>
+    </section>
+    </header>
+   
+   <main>
+    <h1>Latest Posts</h1>
+    <h3>Filter by Category</h3>
+    <div className="filter-btn-box">
+      <button>ALL</button>
+      <button>Artificial Intelligence</button>
+      <button>Cloud Computing</button>
+      <button>DevOps</button>
+      <button>Programming Languages</button>
+      <button>Mobile Application Development</button>
+      <button>Technology and Tools</button>
+      <button>Get a Job in a Tech Company</button>
+      <button>Others</button>
     </div>
-  );
+   </main>
+   <ImageGrid/>
+  </div>
+  
+  )
 }
 
 export default App;
---
+++ src/Component/image.js
@@ -0,0 +1,105 @@
+import React from "react"
+import img1 from '../Image/img1.jpg'
+import img2 from '../Image/img2.jpg'
+import img3 from '../Image/img3.jpg'
+import img4 from '../Image/img4.jpg'
+import img5 from '../Image/img5.jpg'
+import img6 from '../Image/img6.jpg'
+
+function ImageGrid(props){
+  return(
+    <>
+    <section className="first_block">
+    <div className="img_grid">
+       <img src={img1}/>
+       <h2>Fresh graduate or it<br></br> profesional looking for a</h2> 
+       <p className="blue">Arman Ahmad 
+        |5 jul 2019</p> 
+        <p>if you are fresh graduate or new to<br></br> it looking for the next job you need<bR></bR>
+         to read this the last couple of year<br></br> zecklabs give me thea unique<br></br> new opportunity to asses technology</p>
+         </div>
+
+         <div className="img_grid">
+       <img src={img2}/>
+       <h2>Big data importance of Big<br></br> Management and</h2> 
+       <p className="blue">Edyoda
+        |5 jul 2019</p> 
+        <p>if you are fresh graduate or new to<br></br> it looking for the next job you need<bR></bR>
+         to read this the last couple of year<br></br> zecklabs give me thea unique<br></br> new opportunity to asses technology</p>
+         </div>
+
+         <div className="img_grid">
+       <img src={img3}/>
+       <h2>Fresh graduate or it<br></br> profesional looking for a</h2> 
+       <p className="blue">Arman Ahmad 
+        |5 jul 2019</p> 
+        <p>if you are fresh graduate or new to<br></br> it looking for the next job you need<bR></bR>
+         to read this the last couple of year<br></br> zecklabs give me thea unique<br></br> new opportunity to asses technology</p>
+         </div>
+    </section> 
+
+   
+    <section className="first_block">
+    <div className="img_grid">
+       <img src={img4}/>
+       <h2>Fresh graduate or it<br></br> profesional looking for a</h2> 
+       <p className="blue">Arman Ahmad 
+        |5 jul 2019</p> 
+        <p>if you are fresh graduate or new to<br></br> it looking for the next job you need<bR></bR>
+         to read this the last couple of year<br></br> zecklabs give me thea unique<br></br> new opportunity to asses technology</p>
+         </div>
+
+         <div className="img_grid">
+       <img src={img5}/>
+       <h2>Fresh graduate or it<br></br> profesional looking for a</h2> 
+       <p className="blue">Arman Ahmad 
+        |5 jul 2019</p> 
+        <p>if you are fresh graduate or new to<br></br> it looking for the next job you need<bR></bR>
+         to read this the last couple of year<br></br> zecklabs give me thea unique<br></br> new opportunity to asses technology</p>
+         </div>
+
+         <div className="img_grid">
+       <img src={img6}/>
+       <h2>Fresh graduate or it<br></br> profesional looking for a</h2> 
+       <p className="blue">Arman Ahmad 
+        |5 jul 2019</p> 
+        <p>if you are fresh graduate or new to<br></br> it looking for the next job you need<bR></bR>
+         to read this the last couple of year<br></br> zecklabs give me thea unique<br></br> new opportunity to asses technology</p>
+         </div>
+    </section> 
+
+
+    <section className="first_block">
+    <div className="img_grid">
+       <img src={img4}/>
+       <h2>Typical Day of Data<br></br>sentist an insider story</h2> 
+       <p className="blue">SAURABH ghosh
+        |5 jul 2019</p> 
+        <p>I have Been d Scientist for three<br></br>now i can only say the chris<bR></bR>
+         Science are foudation of all the <br></br> that are emerging the world<br></br> new opportunity to asses technology</p>
+         </div>
+
+         <div className="img_grid">
+       <img src={img5}/>
+       <h2>React Native vs Flutter<br></br> Breaking the confusion</h2> 
+       <p className="blue">Edyoda
+        |5 jul 2019</p> 
+        <p>the two biggest social plateform<br></br> creaed the buzz in the application<bR></bR>
+         facebook and flatter are google by<br></br> zecklabs give me thea unique<br></br> new opportunity to asses technology</p>
+         </div>
+
+         <div className="img_grid">
+       <img src={img6}/>
+       <h2>Fresh graduate or it<br></br> profesional looking for a</h2> 
+       <p className="blue">Arman Ahmad 
+        |5 jul 2019</p> 
+        <p>if you are fresh graduate or new to<br></br> it looking for the next job you need<bR></bR>
+         to read this the last couple of year<br></br> zecklabs give me thea unique<br></br> new opportunity to asses technology</p>
+         </div>
+    </section> 
+      
+      </>
+  )
+}
+
+export default ImageGrid
